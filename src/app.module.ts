b81ae4e3b30acd13de604096457f85f648c501e0
Nikita Kazantsev <nkaz2003@gmail.com>
2025-04-10 16:57:49 +0300

    increase cache ttl

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,7 +39,10 @@ import { PrismaReadService, PrismaService } from './prisma.service'
 			isGlobal: true,
 		}),
 		CacheModule.register({
-			ttl: process.env.NODE_ENV === 'development' ? 1000 : 1000 * 60 * 60,
+			ttl:
+				process.env.NODE_ENV === 'development'
+					? 1000
+					: 1000 * 60 * 60 * 24 * 7,
 			max: 1000,
 			isGlobal: true,
 		}),
